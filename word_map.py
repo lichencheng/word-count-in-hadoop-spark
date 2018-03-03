90c4b2b4c195fcfc8a34ac8c6572cb66dde6052a
lichencheng <lichencheng4225288@gmail.com>
2018-03-03 14:11:48 +0800

    Create word_map.py

---
+++ word_map.py
@@ -0,0 +1,21 @@
+import sys
+
+from operator import itemgetter
+from itertools import groupby
+
+def read_mapper_output(file, separator='\t'):
+    for line in file:
+        yield line.rstrip().split(separator)
+
+def main(separator='\t'):
+    data = read_mapper_output(sys.stdin, separator=separator)
+    for current_word, group in groupby(data, itemgetter(0)):
+        #try:
+        total_count = sum(int(count) for current_word, count in group)
+        print "%s%s%d" % (current_word, separator, total_count)
+        #except ValueError:
+        #    # count was not a number, so silently discard this item
+        #    pass
+
+if __name__ == "__main__":
+    main()
